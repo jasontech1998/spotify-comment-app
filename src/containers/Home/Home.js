f9fccb6e6b2df6ee5bfa5240350b739ec02238c8
jasontech1998 <jasontech1998@gmail.com>
2020-10-22 19:37:17 -0700

    Added lottie to home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,11 +4,21 @@ import './Home.css';
 import {loginUrl} from '../../spotify';
 import Search from '../../components/Search/Search';
 
+import Lottie from 'react-lottie';
+import animationData from '../../components/Images/audiowave';
+
 
 class Home extends Component {
-
   render() {
     
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+    };
     let signOrSearch = (
       <div id="logInBtn">
         <button type="button" className="btn"><a href={loginUrl} id="logBtn">Login with Spotify </a></button>
@@ -27,7 +37,11 @@ class Home extends Component {
           <i className="fab fa-spotify"></i>
           <h1 id="appName" style={{marginBottom: "0px", marginLeft: "8px"}}>PodSpot</h1>
         </div>
-        <h4 id="bio">Listen, comment, and rate your favorite podcasts.</h4>
+        <h4 id="bio">Listen, comment and rate your favorite podcasts.</h4>
+        <Lottie 
+          options={defaultOptions}
+          width={200}
+          />
         {signOrSearch}
       </div>
     );
